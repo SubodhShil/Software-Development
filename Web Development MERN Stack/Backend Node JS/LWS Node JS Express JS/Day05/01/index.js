28e04c096fb398e778405233ffd9f66ade650bc9
Subodh Chandra Shil <golang602@gmail.com>
2024-12-11 11:08:35 +0600

    updated

--- Web Development MERN Stack/Backend Node JS/LWS Node JS Express JS/Day05/01/index.js
+++ Web Development MERN Stack/Backend Node JS/LWS Node JS Express JS/Day05/01/index.js
@@ -5,13 +5,17 @@ const app = express();
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-    console.log(res.headersSent);
+    /* console.log(res.headersSent);
     res.render('pages/about', {
         name: 'House of the death',
     });
-    console.log(res.headersSent);
+    console.log(res.headersSent); */
+
+    res.cookie('userName', "Subodh");
+    res.end();
 });
 
 app.listen(PORT, (req, res) => {
     console.log('Server is running');
 });
+
