ceea697607625bbb54bdaea3011c31b380073c60
Subodh Chandra Shil <golang602@gmail.com>
2024-12-30 08:55:10 +0600

    Backend day 02 added

--- Web Development MERN Stack/Backend Node JS/Learnings Milestone/Day02/app.js
+++ Web Development MERN Stack/Backend Node JS/Learnings Milestone/Day02/app.js
@@ -8,6 +8,7 @@ app.get('/', (req, res) => {
     res.send("Working");
 });
 
+// C => Create 
 app.get('/create', async (req, res) => {
     let createdUser = await usermodel.create({
         name: "Subodh",
@@ -18,6 +19,30 @@ app.get('/create', async (req, res) => {
     res.send(createdUser);
 });
 
+// R => Read 
+
+app.get('/read', async (req, res) => {
+    let users = await usermodel.find();
+    res.send(users);
+
+    // let singleUser = await usermodel.find({ username: "Subodh" });
+    // res.send(singleUser);
+})
+
+// U => Update 
+app.get('/update', async (req, res) => {
+
+    let updatedUser = await usermodel.findOneAndUpdate({ username: "sub003" }, { username: "Subodh" }, { new: true });
+
+    res.send(updatedUser);
+});
+
+// D => Delete
+app.get('/delete', async (req, res) => {
+    let deletedUser = await usermodel.findOneAndDelete({ username: "Subodh" });
+    res.send(deletedUser);
+})
+
 app.listen(PORT, () => {
     console.log('Running good');
-})
+});
