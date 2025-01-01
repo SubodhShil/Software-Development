39c5d95d576dd53598f7d2f47af1ae404bc36ad5
Subodh Chandra Shil <golang602@gmail.com>
2025-01-02 00:10:18 +0600

    updated

--- Web Development MERN Stack/Backend Node JS/Learnings Milestone/Day05/index.js
+++ Web Development MERN Stack/Backend Node JS/Learnings Milestone/Day05/index.js
@@ -1,16 +1,20 @@
 const express = require("express");
 const jwt = require("jsonwebtoken");
 const app = express();
+const cookieParser = require("cookie-parser");
+
+app.set("view engine", "ejs");
+
+app.use(express.json());
+app.use(express.urlencoded({ extends: true }));
+app.use(express.static("public"));
+app.use(cookieParser());
 
 app.get("/", (req, res) => {
-    res.json({
-        message: "A simple API",
-    })
+    res.render("index");
 });
 
 const PORT = 5000;
 app.listen(PORT, () => {
     console.log(`App is running on: http://localhost:${PORT}`);
 });
-
-
