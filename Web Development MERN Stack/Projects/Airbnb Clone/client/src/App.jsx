63a93ef3b1648befbfe67fc66167a59c942ed673
Subodh Chandra Shil <golang602@gmail.com>
2025-02-05 22:26:59 +0600

    updated

---
+++ Web Development MERN Stack/Projects/Airbnb Clone/client/src/App.jsx
@@ -0,0 +1,55 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+    return (
+        <>
+            <header>
+                {/* icon and company name */}
+                <a href="" className="flex items-center gap-2">
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        strokeWidth={1.5}
+                        stroke="currentColor"
+                        className="size-8 -rotate-90"
+                    >
+                        <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            d="M6 12 3.269 3.125A59.769 59.769 0 0 1 21.485 12 59.768 59.768 0 0 1 3.27 20.875L5.999 12Zm0 0h7.5"
+                        />
+                    </svg>
+
+                    <span className="text-2xl">aircng</span>
+                </a>
+
+                {/* Header Search bar */}
+                <div>
+                    <div>Anywhere</div>
+                    <div>Any Week</div>
+                    <div>Add Guests</div>
+                    <button>
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            strokeWidth={1.5}
+                            stroke="currentColor"
+                            className="size-6"
+                        >
+                            <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z"
+                            />
+                        </svg>
+                    </button>
+                </div>
+            </header>
+        </>
+    );
+}
+
+export default App;
