2a990294ff2c50a7738d1aa820578692cbf15fff
Subodh Chandra Shil <golang602@gmail.com>
2025-03-09 22:57:40 +0600

    C# practices added

--- Programming Languages/C#/Day 10/Program.cs
+++ Programming Languages/C#/Day 10/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 
 public class Program
 {
@@ -6,7 +7,12 @@ public class Program
     {
         // f1();
         // f2();
-        f3();
+        // f3();
+        // f4();
+        // f5();
+        // f6();
+        // f7();
+        f8();
     }
 
     // string formatting
@@ -69,5 +75,78 @@ public class Program
             System.Console.Write(character);
             System.Threading.Thread.Sleep(500); // shows next character after 500 ms or .5 sec
         }
+    }
+
+    public static void f4()
+    {
+        // check if a substring presents
+        string text = "Here are you";
+        System.Console.WriteLine(text.Contains("are"));
+
+        // check if string is null or empty
+        // text = "";
+        text = null;
+
+        if (text == "")
+            System.Console.WriteLine("Show");
+        else
+            System.Console.WriteLine("Not show");
+
+        // if (string.IsNullOrEmpty(text))
+        //     System.Console.WriteLine("Empty string");
+        // else
+        //     System.Console.WriteLine("Not empty");
+    }
+
+    public static void f5()
+    {
+        string text = "Rotten world";
+        System.Console.WriteLine(text.Substring(0, 6));
+    }
+
+    public static void f6()
+    {
+        // character string
+        char[] charStr = { 'I', ' ', 'W', 'i', 'n' };
+        System.Console.WriteLine(charStr);
+        System.Console.WriteLine(charStr.GetType());
+
+        // converting into string
+        string str1 = new string(charStr);
+        System.Console.WriteLine(str1.GetType());
+    }
+
+    // Simple password matching
+    public static void f7()
+    {
+        System.Console.Write("Enter your password: ");
+        string password1 = Console.ReadLine();
+
+        System.Console.Write("Enter password again: ");
+        string password2 = Console.ReadLine();
+
+        if (
+            !password1.Equals(string.Empty)
+            && !password2.Equals(string.Empty)
+            && password1.Equals(password2)
+        )
+        {
+            System.Console.WriteLine("Password matched");
+        }
+        else
+        {
+            System.Console.WriteLine("Password didn't matched");
+        }
+    }
+
+    /* StringBuilder */
+    public static void f8()
+    {
+        StringBuilder sb1 = new StringBuilder("Subodh", 10);
+        System.Console.WriteLine($"{sb1.ToString()}\nSize: {sb1.Length}");
+
+        sb1.Append(" Chandra Shil");
+        Console.WriteLine(sb1.ToString());
+        Console.WriteLine(sb1.Length);
     }
 }
